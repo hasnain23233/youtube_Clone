55cb2e818388d15f9969741bb2095c2cbfb44348
Hasnain Khushi <hasnainkhushii@gmail.com>
2025-09-18 17:25:57 +0500

    connecting to the mongodb

--- server/index.js
+++ server/index.js
@@ -3,6 +3,10 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const app = express();
 const port = 5000;
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+const mongoDBLink = process.env.MONGO_URL
 
 app.use(cors());
 app.use(bodyParser.json());
@@ -12,6 +16,12 @@ app.use('/', (req, res, next) => {
     res.send('Server is running');
 })
 
-app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port} this is a youtube clone project`);
+
+mongoose.connect(mongoDBLink, { dbName: "youtube_clone", useNewUrlParser: true, useUnifiedTopology: true }).then(() => {
+    app.listen(port, () => {
+        console.log(`Server is running on http://localhost:${port} this is a youtube clone project`);
+    })
+}).catch((err) => {
+    console.log(err.message);
+    console.log("Error while connecting to mongoDB");
 })
