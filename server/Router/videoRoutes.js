00a5406c52071802c8dcd4f0083059f21affa3e5
Hasnain Khushi <hasnainkhushii@gmail.com>
2025-09-19 17:25:12 +0500

    update the beckend code and add the edit feature in the application of the project

--- server/Router/videoRoutes.js
+++ server/Router/videoRoutes.js
@@ -1,10 +1,31 @@
 const express = require("express");
 const router = express.Router();
 const upload = require("../middleware/uploadMiddleware");
-const { uploadVideo, getVideos, getVideoById } = require("../Conroller/videoController");
+const {
+    uploadVideo,
+    getVideos,
+    getVideoById,
+    getUserVideos,
+    updateVideo,
+    deleteVideo,
+} = require("../Controller/videoController");
 
+// Upload
 router.post("/upload", upload.single("video"), uploadVideo);
+
+// Get all videos
 router.get("/", getVideos);
+
+// Get single video
 router.get("/:id", getVideoById);
+
+// Get all videos of a user
+router.get("/user/:userId", getUserVideos);
+
+// Update video
+router.put("/:id", updateVideo);
+
+// Delete video
+router.delete("/:id", deleteVideo);
 
 module.exports = router;
