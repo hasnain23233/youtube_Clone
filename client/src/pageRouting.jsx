a6bd0f16aae48de01b1bd01d0504606eff23ce65
Hasnain Khushi <hasnainkhushii@gmail.com>
2025-09-19 17:19:41 +0500

    update the code of the project

--- client/src/pageRouting.jsx
+++ client/src/pageRouting.jsx
@@ -5,12 +5,13 @@ import {
     Link
 } from "react-router-dom";
 import UploadVideo from './adminRollPages/UploadVideo';
+import MyVideos from './adminRollPages/MyVideos';
 
 const pageRouting = () => {
     return (
         <div>
             <Routes>
-                <Route path="/myvideos" element={<h1>My Videos</h1>} />
+                <Route path="/channels" element={<MyVideos />} />
                 <Route path="/upload" element={<UploadVideo />} />
                 <Route path="/allusers" element={<h1>All Users</h1>} />
                 <Route path="/allvideos" element={<h1>All Videos</h1>} />
