d444c3c053d540377d9bcd8e7fd0417d86f31583
Hasnain Khushi <hasnainkhushii@gmail.com>
2025-09-19 16:26:56 +0500

    update the routing page of the project

--- client/src/FullStackYoutubeFile.jsx
+++ client/src/FullStackYoutubeFile.jsx
@@ -1,13 +1,19 @@
 import React from 'react'
 import Navbar from './components/navbar'
 import SideNavbar from './components/Sidenavbar'
+import PageRouting from './pageRouting'
 
 const FullStackYoutubeFile = () => {
     return (
         <div>
             <Navbar />
-            <div>
-                <SideNavbar />
+            <div className=' mt-2 flex gap-10'>
+                <div className='border w-2/12'>
+                    <SideNavbar />
+                </div>
+                <div>
+                    <PageRouting />
+                </div>
             </div>
         </div>
     )
---
+++ client/src/pageRouting.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const pageRouting = () => {
+    return (
+        <div>
+            <h1 className='text-white'>this is a main page routing page of the project</h1>
+        </div>
+    )
+}
+
+export default pageRouting
