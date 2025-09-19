447981b3952458f16b17f1ed5cd16cbf60ed7c42
Hasnain Khushi <hasnainkhushii@gmail.com>
2025-09-19 16:55:10 +0500

    storing the video and details in the beckend database of the website

--- client/src/pageRouting.jsx
+++ client/src/pageRouting.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
+import {
+    Routes,
+    Route,
+    Link
+} from "react-router-dom";
+import UploadVideo from './adminRollPages/UploadVideo';
 
 const pageRouting = () => {
     return (
         <div>
-            <h1 className='text-white'>this is a main page routing page of the project</h1>
+            <Routes>
+                <Route path="/myvideos" element={<h1>My Videos</h1>} />
+                <Route path="/upload" element={<UploadVideo />} />
+                <Route path="/allusers" element={<h1>All Users</h1>} />
+                <Route path="/allvideos" element={<h1>All Videos</h1>} />
+            </Routes>
         </div>
     )
 }
